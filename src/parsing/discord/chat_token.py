15e96b039d17e1979ad9879a73d7e3a18c3c2d24
zmbarson <zmbarson@gmail.com>
2021-11-07 19:21:00 -0600

    Update chat_token.py

--- src/parsing/discord/chat_token.py
+++ src/parsing/discord/chat_token.py
@@ -3,7 +3,7 @@ from collections import namedtuple
 
 """
 
-    Represents a token parsed from a discord chat message,
+    Represents a token parsed from a Discord chat message,
     as defined by its TokenType and source text.
 
 """
